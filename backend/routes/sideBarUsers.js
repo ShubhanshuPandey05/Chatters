f0978cc1f9a0737eca0379b27cde1a1a0ec930d4
Shubhanshu Pandey <159635438+ShubhanshuPandey05@users.noreply.github.com>
2024-03-06 17:38:35 +0530

    Delete backend directory

--- backend/routes/sideBarUsers.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protectRoute from "../middleware/protectRoute.js";
-import { getUserForSideBar } from "../controllers/sideBarUserController.js";
-
-const router = express.Router();
-
-router.get("/",protectRoute, getUserForSideBar);
-
-export default router;
