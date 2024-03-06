e202bff0a4635b6a94e0253e9c81a4643a48f787
Shubhanshu Pandey <159635438+ShubhanshuPandey05@users.noreply.github.com>
2024-03-06 17:43:08 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatters-svk4.onrender.com",
         methods: ["GET", "POST", "DELETE"]
     },
 })
